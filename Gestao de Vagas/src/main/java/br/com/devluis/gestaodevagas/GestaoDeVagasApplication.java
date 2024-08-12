c79d631da2ba3ea6c654f5aa18a67406b786619c
luisericlima <luisericlima12@outlook.com>
2024-08-12 15:53:24 -0300

    criacao projeto

---
+++ Gestao de Vagas/src/main/java/br/com/devluis/gestaodevagas/GestaoDeVagasApplication.java
@@ -0,0 +1,13 @@
+package br.com.devluis.gestaodevagas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeVagasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDeVagasApplication.class, args);
+	}
+
+}
