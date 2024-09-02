987db27cc4483fd34c798173ae19b8e117fb83e4
luisericlima <luisericlima12@outlook.com>
2024-09-02 16:36:26 -0300

    colocado docker-compose e configuracao do JPA

--- Gestao de Vagas/src/main/java/br/com/devluis/gestaodevagas/modules/candidate/CandidateEntity.java
+++ Gestao de Vagas/src/main/java/br/com/devluis/gestaodevagas/modules/candidate/CandidateEntity.java
@@ -1,20 +1,25 @@
 package br.com.devluis.gestaodevagas.modules.candidate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Entity(name = "candidate")
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class CandidateEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
 
@@ -30,4 +35,6 @@ public class CandidateEntity {
 
     private String description;
     private String curriculum;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
--- Gestao de Vagas/src/main/java/br/com/devluis/gestaodevagas/modules/candidate/controllers/Controller.java
+++ Gestao de Vagas/src/main/java/br/com/devluis/gestaodevagas/modules/candidate/controllers/Controller.java
@@ -2,6 +2,8 @@ package br.com.devluis.gestaodevagas.modules.candidate.controllers;
 
 import br.com.devluis.gestaodevagas.modules.candidate.CandidateEntity;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
 
     @PostMapping("/")
-    public void create(@Valid @RequestBody CandidateEntity candidate){
+    public ResponseEntity<CandidateEntity> create(@Valid @RequestBody CandidateEntity candidate){
         System.out.println("Candidato: " + candidate);
         System.out.println(candidate.getEmail());
+        return ResponseEntity.status(HttpStatus.CREATED).body(candidate);
 
     }
 }
