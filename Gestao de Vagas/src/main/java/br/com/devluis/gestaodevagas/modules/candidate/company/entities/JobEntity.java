3a8a156284178b14a637d2f380dc072571fd0853
luisericlima <luisericlima12@outlook.com>
2024-09-04 17:12:49 -0300

    criacao da entidade Job relacionando com Company

---
+++ Gestao de Vagas/src/main/java/br/com/devluis/gestaodevagas/modules/candidate/company/entities/JobEntity.java
@@ -0,0 +1,33 @@
+package br.com.devluis.gestaodevagas.modules.candidate.company.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity(name = "job")
+public class JobEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String description;
+    private String level;
+
+    @ManyToOne()
+    @JoinColumn(name = "company_id")
+    private CompanyEntity company;
+
+    @Column(name = "company_id")
+    private UUID companyId = getCompanyId();
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+
+
+}
